34d08645cf10d783feb935059ae470e7084a60aa
ixrocky <shivamlkr629@gmail.com>
2025-09-25 04:22:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/abh628/kirtib",
+    "https://github.com/ixrocky/kirtib",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
